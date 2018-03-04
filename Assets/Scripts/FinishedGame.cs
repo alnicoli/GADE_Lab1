5b02b8ef0abc59f9ccbf044bdf6f8d8246090a5c
Alessandro <chratzchopf@hotmail.com>
2018-03-04 15:33:33 +0100

    Added Level, lightning, goal

---
+++ Assets/Scripts/FinishedGame.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishedGame : MonoBehaviour {
+	private void OnTriggerEnter(Collider other) {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+	}
+}
