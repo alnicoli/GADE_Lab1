267fda4f695d534fba7357dda581b8705c91e93d
Alessandro Nicoli <chratzchopf@hotmail.com>
2018-03-05 19:51:11 +0100

    Added second stage

--- Assets/Scripts/FinishedGame.cs
+++ Assets/Scripts/FinishedGame.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class FinishedGame : MonoBehaviour {
 	private void OnTriggerEnter(Collider other) {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+		if (SceneManager.GetActiveScene().name == "Stage1") {
+			SceneManager.LoadScene("Stage2");
+		}
+		else if (SceneManager.GetActiveScene().name == "Stage2") {
+			SceneManager.LoadScene("Stage1");
+		}
 	}
 }
